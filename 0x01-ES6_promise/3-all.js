3f6d05f78ac1b7f182d3d2edbdca32a48d22936a
Daniel Enagu <daniel@cygnusspin.com>
2023-06-28 16:59:18 +0100

    Fix code error for task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,13 @@ import { uploadPhoto, createUser } from './utils';
 
 // const testReject = Promise.reject();
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+const handleProfileSignup = () => {
+  const promises = [uploadPhoto(), createUser()];
+  return Promise.all(promises)
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
       return values;
     }).catch(() => { console.log('Signup system offline'); });
-}
+};
+
+export default handleProfileSignup;
