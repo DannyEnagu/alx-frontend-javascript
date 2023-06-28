6cf903bec93148e0f18042f141805f3738f8784b
Daniel Enagu <daniel@cygnusspin.com>
2023-06-28 19:47:04 +0100

    FIx task 6 response structure.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,7 +7,6 @@ const handleProfileSignup = () => {
   return Promise.all(promises)
     .then((values) => {
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-      return values;
     }).catch(() => { console.log('Signup system offline'); });
 };
 
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,17 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises)
-    .then((result) => result)
-    .catch(() => {});
+    .then((result) => {
+      const data = [];
+
+      for (const xy of result) {
+        const obj = {
+          status: xy.status,
+          value: xy.value || xy.reason,
+        };
+        data.push(obj);
+      }
+
+      return data;
+    });
 }
