a812373bfe8cb96857fb952dea0a1a13b2a0b00c
Daniel Enagu <daniel@cygnusspin.com>
2023-06-28 16:34:24 +0100

    Fix checker error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,6 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
+      return values;
     }).catch(() => { console.log('Signup system offline'); });
 }
